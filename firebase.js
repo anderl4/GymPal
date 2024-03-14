62818496c93ca11ba0f622274385989f26a49261
Kevin Tran <kevintr303@gmail.com>
2024-03-14 12:18:35 -0700

    Fix persistence on android

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID} from '@env';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence, browserSessionPersistence } from 'firebase/auth';
+import { Platform } from 'react-native';
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -12,7 +15,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const persistence = Platform.OS === 'web'
+           ? browserSessionPersistence
+           : getReactNativePersistence(ReactNativeAsyncStorage);
+const auth = initializeAuth(app, {persistence});
 
 export { app, auth };
 
